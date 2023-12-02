a58769c0ec6174317cccbd59f6a2b820b9c454ec
Roger <roger@1bits.cloud>
2023-12-02 22:46:58 +0100

    solution for day 2, star 2

--- AoC2023Day2/Program.cs
+++ AoC2023Day2/Program.cs
@@ -1,7 +1,12 @@
-﻿using System.Text.RegularExpressions;
+﻿using AoC2023Day2;
+using System.Text.RegularExpressions;
 
 internal class Program
 {
+    public static readonly string Red = "red";
+    public static readonly string Green = "green";
+    public static readonly string Blue = "blue";
+
     private static void Main(string[] args)
     {
         Dictionary<int, string[]> games = ReadInput();
@@ -27,15 +32,15 @@ internal class Program
                 {
                     var number = Convert.ToInt32(Regex.Match(set, @"\d+").Value);
 
-                    if (set.Contains("red") && number > 12)
+                    if (set.Contains(Red) && number > 12)
                     {
                         possible = false;
                     }
-                    if (set.Contains("green") && number > 13)
+                    if (set.Contains(Green) && number > 13)
                     {
                         possible = false;
                     }
-                    if (set.Contains("blue") && number > 14)
+                    if (set.Contains(Blue) && number > 14)
                     {
                         possible = false;
                     }
@@ -64,6 +69,47 @@ internal class Program
     private static void SolveStar2(Dictionary<int, string[]> games)
     {
         var star2 = 0;
+
+        foreach (var game in games)
+        {
+            List<SubSet> subsets = new();
+
+            foreach (var subset in game.Value)
+            {
+                var sets = subset.Split(',');
+
+                foreach (var set in sets)
+                {
+                    var t = set.TrimStart().Split(' ');
+
+                    subsets.Add(new SubSet(Convert.ToInt32(t[0]), t[1]));
+                }
+
+                subsets = subsets.OrderByDescending(o => o.Number).ToList();
+            }
+
+            var highR = 0;
+            var highG = 0;
+            var highB = 0;
+
+            for (int i = 0; i < subsets.Count; i++)
+            {
+                if (subsets[i].Color == Red && subsets[i].Number > highR)
+                {
+                    highR = subsets[i].Number;
+                }
+                if (subsets[i].Color == Green && subsets[i].Number > highG)
+                {
+                    highG = subsets[i].Number;
+                }
+                if (subsets[i].Color == Blue && subsets[i].Number > highB)
+                {
+                    highB = subsets[i].Number;
+                }
+            }
+
+            star2 += (highR * highG * highB);
+        }
 
         Console.WriteLine($"total star 2: {star2}");
     }
---
+++ AoC2023Day2/SubSet.cs
@@ -0,0 +1,14 @@
+﻿namespace AoC2023Day2
+{
+    internal class SubSet
+    {
+        public int Number { get; set; }
+        public string Color { get; set; }
+
+        public SubSet(int number, string color)
+        {
+            Number = number;
+            Color = color;
+        }
+    }
+}
